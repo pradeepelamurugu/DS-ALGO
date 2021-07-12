f2792bad7e17cf7c997a7a435a27c1c417a00035
Pradeep Elamurugu <56883812+pradeepelamurugu@users.noreply.github.com>
2021-07-12 15:18:42 +0530

    created by bot

---
+++ Leaders in an array.py
@@ -0,0 +1,18 @@
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n){
+        // Code here
+        vector<int> ans;
+        int max = a[n-1];
+        ans.push_back(max);
+        for(int i=n-2;i>=0;i--){
+            if(max<=a[i]){
+                ans.push_back(a[i]);
+                max=a[i];
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
